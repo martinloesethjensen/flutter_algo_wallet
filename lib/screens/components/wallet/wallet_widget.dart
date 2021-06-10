3e0ded0c04417d33f683e78e9da7ef0ff446227d
Martin L. Jensen <martinloesethjensen@gmail.com>
2021-06-10 17:59:27 +0200

    Added functionality to the recieve button. Now it shows a QR and has ability to copy public address

--- lib/screens/components/wallet/wallet_widget.dart
+++ lib/screens/components/wallet/wallet_widget.dart
@@ -1,5 +1,6 @@
 import 'package:algorand_dart/algorand_dart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_algo_wallet/bottom_navbar/bottom_navbar_provider.dart';
 import 'package:flutter_algo_wallet/global_providers/account/account_provider.dart';
 import 'package:flutter_algo_wallet/screens/dashboard/dashboard_screen_provider.dart';
 import 'package:flutter_algo_wallet/theme/theme.dart';
@@ -16,6 +17,7 @@ class WalletWidget extends StatelessWidget {
     final algorand = context.watch<Algorand>();
     final dashboardScreenMode = context.watch<DashboardScreenModeProvider>();
     final account = context.watch<AccountProvider>();
+    final navBarProvider = context.watch<BottomNavigationBarProvider>();
 
     return Padding(
       padding: const EdgeInsets.all(paddingSizeDefault),
@@ -37,6 +39,11 @@ class WalletWidget extends StatelessWidget {
               );
 
               ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+              // If we create a new wallet from the wallet tab (1) then we should navigate to the dashboard
+              if (navBarProvider.currentIndex != 0) {
+                navBarProvider.currentIndex = 0;
+              }
             },
             child: Text('Create a new wallet'),
             style: ButtonStyle(
@@ -50,7 +57,25 @@ class WalletWidget extends StatelessWidget {
             message: 'Currently unavailable!',
             child: ElevatedButton(
               key: Key('IMPORT_WALLET_BUTTON'),
-              onPressed: null, // TODO
+              onPressed: () async {
+                final restoredAccount =
+                    await Account.fromSeedPhrase(seedPhrase);
+                account.account = restoredAccount;
+                dashboardScreenMode.currentWalletStatus =
+                    DashboardScreenMode.LOADED_WALLET;
+
+                final snackBar = SnackBar(
+                  behavior: SnackBarBehavior.floating,
+                  content: Text('Imported test wallet.'),
+                );
+
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                // If we import a wallet from the wallet tab (1) then we should navigate to the dashboard
+                if (navBarProvider.currentIndex != 0) {
+                  navBarProvider.currentIndex = 0;
+                }
+              },
               child: Text('Import existing wallet'),
               style: ButtonStyle(
                 fixedSize: MaterialStateProperty.all<Size>(
@@ -64,3 +89,37 @@ class WalletWidget extends StatelessWidget {
     );
   }
 }
+
+// TODO: Just for testing
+// Random Algorand public address
+final publicAddress =
+    'D3ZUVRXN2KN44O6Z335HI6SOY7WLQW3RNP7HPGTAXBKZWPWKKOZDKJJT5Q';
+
+// Random Algorand seed phrase
+final seedPhrase = <String>[
+  'certain',
+  'social',
+  'kidney',
+  'magic',
+  'special',
+  'replace',
+  'genuine',
+  'meadow',
+  'pulse',
+  'tennis',
+  'unable',
+  'obey',
+  'urban',
+  'clerk',
+  'domain',
+  'sock',
+  'belt',
+  'slam',
+  'hybrid',
+  'tank',
+  'lumber',
+  'reason',
+  'canvas',
+  'about',
+  'wink'
+];
